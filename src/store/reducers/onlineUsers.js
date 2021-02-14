b192b67ed69442253c6c0ff3768c4e8dd9ce44b3
Solaire003 <vadin-abramov-00@mail.ru>
2021-02-14 20:36:42 +0300

    fix: new user

--- src/store/reducers/onlineUsers.js
+++ src/store/reducers/onlineUsers.js
@@ -5,10 +5,11 @@ export function getOnlineUsers(state = initialState, {payload, type}) {
         case "GET_USERS":
             return {...state, list: payload.users};
         case "NEW_USER":
-            if (state.list.find(user => user._id !== payload.user.id)){
-                return {...state, list: [...state.list, payload.user]};
+            if (state.list.find(user => user._id == payload.user.id)){
+                return state;
             }
-            return state;
+            return {...state, list: [...state.list, payload.user]};
+
         case "LEAVE_USER":
             return {...state, list: state.list.slice().filter((user)=> user._id !== payload.user.id)}
         default:
